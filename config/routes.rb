2c26810213dd3fc60d5b1ed38674b575c2d3c8d1
Kenn K. Lau <kenn.lau.design@gmail.com>
2013-08-03 22:14:06 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"
+
+  # option for root
+  # match '/', to: 'static_pages#home'
+  # below is more conventional for root path
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  # automatically creates
+  # about_path => '/about'
+  # about_url => 'http://localhost:3000/about'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
